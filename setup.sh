96b2ff28629e8018e55e4108bb361d7938a9403e
Alexander Reelsen <alexander@reelsen.net>
2020-11-12 10:23:33 +0100

    Update Elastic Stack to 7.10.0

--- setup.sh
+++ setup.sh
@@ -13,7 +13,7 @@ echo "deb http://deb.debian.org/debian buster-backports main" | sudo tee /etc/ap
 
 sudo apt-get update 
 
-STACK_VERSION=7.9.3
+STACK_VERSION=7.10.0
 
 sudo apt-get install -y vim auditbeat=$STACK_VERSION elasticsearch=$STACK_VERSION kibana=$STACK_VERSION filebeat=$STACK_VERSION firejail strace crystal libssl-dev libxml2-dev libyaml-dev libgmp-dev libreadline-dev libz-dev auditd
 
